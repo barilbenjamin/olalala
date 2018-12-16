04c55e9a4dcb681145f7341aa4ed8706a9eea190
aijd)a^)$dafz^=$f*ùz <38180601+zadadzafdz@users.noreply.github.com>
2018-12-16 17:34:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const Discord = require("discord.js");
 try {
     const bot = new Discord.Client();
-
+    var prefix = ("*");
 
 bot.on("ready", () => {
     console.log(bot.user.id)
 })
+bot.on("guildCreate", guild => {
+    let embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setAuthor("Kais bot ", bot.user.avatarURL)
+    .setDescription("Découvre la commande pour rendre plus chill vos roles avec *help !")
+    .setTimestamp()
+    guild.send(embed)
+})
 bot.on("message", message => {
-    if (message.content.startsWith('*rainbow')) {
+    if (message.content.startsWith(prefix+'rainbow')) {
         let mention = message.mentions.roles.first()
         if(message.channel.type === "dm" || message.channel.type === "group") return;
-        else if(message.mentions.roles.size === 0) return message.channel.send("Usage valide : `*rainbow <@mentiondurolearendrerainbow>`");
-        else if(!message.guild.member(bot.user).hasPermission("MANAGE_ROLES") || !message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("Permission insuffisante du bot ou de vous (Permission requise : `MANAGE_ROLE`");
-        else if(mention.position > message.guild.member(bot.user).highestRole.position || mention.position > message.member.highestRole.position) return message.channel.send("Le role du bot / votre role est en dessous de celui mentionné");
+        else if(message.mentions.roles.size === 0) return message.channel.send(":x: **Usage valide :** `*rainbow <@mentiondurolearendrerainbow>`");
+        else if(!message.guild.member(bot.user).hasPermission("MANAGE_ROLES") || !message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(":x: **Vous ou le bot manquez de perm (Permission requise : ** `MANAGE_ROLE`");
+        else if(mention.position >= message.guild.member(bot.user).highestRole.position || mention.position >= message.member.highestRole.position) return message.channel.send(":x: **Le role du bot / votre role est en dessous ou égal de celui mentionné**");
         else{
-            setInterval(function () {mention.setColor('RANDOM')}, 3000);}
+            message.channel.send(":white_check_mark: **C'est partis :]**")
+            setInterval(function () {mention.setColor('RANDOM')}, 4000);}
+    }
+    if(message.content.startsWith(prefix+"help")){
+        let embed1 = new Discord.RichEmbed()
+        .setAuthor("Commande Rainbow ", bot.user.avatarURL)
+        .setDescription("`*rainbow <@mentiondurolearendremulticolore>` - **Rend multicolore un role** *(Note : pour mentionner un role , il faut l'activer dans les parametres de celui ci)*")
+        .setColor("RANDOM")
+        message.channel.send(embed1)
     }
 })
 
